764f6bcece0204b512736ec3c3712ad5b58f4783
sanikani <sanikani@naver.com>
2026-02-11 20:15:32 +0900

    feat: SolutionRepository 추가 (#5)

---
+++ src/main/java/org/sani/algolog/domain/solution/repository/SolutionRepository.java
@@ -0,0 +1,10 @@
+package org.sani.algolog.domain.solution.repository;
+
+import org.sani.algolog.domain.solution.entity.Solution;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SolutionRepository extends JpaRepository<Solution, Long> {
+    List<Solution> findAllByMemberId(Long memberId);
+}
